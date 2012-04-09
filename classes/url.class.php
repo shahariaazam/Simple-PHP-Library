d7e4a501e2c11badfeb8287416c9bcb359643473
Brian <dbrian89@yahoo.com>
2012-04-09 10:26:32 +0300

    Reverted changes

--- classes/url.class.php
+++ classes/url.class.php
@@ -85,7 +85,6 @@ class URL
         $this->_options['site_root']      = '';
         $this->_options['site_root_ssl']  = '';
         $this->_options['page_token']     = 'goto';
-        $this->_options['action_token']   = 'action';
         $this->_options['get_params']     = array();
         $this->_options['rewrite']        = false;
         $this->_options['secure']         = false;
