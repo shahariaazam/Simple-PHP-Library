9caad86f4a2b5dde28ad26647a2e6386178245f6
Brian <dbrian89@yahoo.com>
2012-06-11 18:22:01 +0300

    Renamed files for easier autoloading

--- autoloader.php
+++ autoloader.php
@@ -2,31 +2,34 @@
 // Defining the class autoloader
 if(!function_exists('class_loader'))
 {
-    function class_loader($class)
+    function class_loader($filename)
     {
         // List of loaded classes
         static $loaded = array();
 
         // Checking if the class was already loaded or not
-        if(!isset($loaded[$class]))
+        if(!isset($loaded[$filename]))
         {
             // Directories where to look for the classes
             $directories = array('classes');
 
             // Formating the class name
-            $class_filename = strtolower($class) . '.class.php';
+            $file = $filename . '.php';
+
+            // Fixing the class file name
+            $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
 
             // Going through the directories where classes might be
             foreach($directories as $directory)
             {
                 // Building the file path
-                $file_path = $directory . DIRECTORY_SEPARATOR . $class_filename;
+                $file_path = $directory . DIRECTORY_SEPARATOR . $file;
 
                 // Checking if a file exists for the requested class
                 if(is_file($file_path))
                 {
                     // Adding the file to the loaded array
-                    $loaded[$class] = $file_path;
+                    $loaded[$filename] = $file_path;
 
                     // Loading the file
                     require $file_path;
--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
+
+// Error reporting
+error_reporting(E_ALL ^ E_NOTICE);
+ini_set('display_errors', true);
+
 // Getting the autoloader
 require 'autoloader.php';
 
 // Getting the functions
 require 'functions/common.inc.php';
 
+// Used namespaces
+use \Database\Database as DB;
+
 
 /////////////////////////////////////////////////////////////////////////////////////
 // AREA 53
 ////////////////////////////////////////////////////////////////////////////////////
+
+$db = DB::init(array('type' => 'mysql_i'));
