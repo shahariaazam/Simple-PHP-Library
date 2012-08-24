2ac2f703f631ef6e93e2d6e6b7526eaed38f345f
brian978 <dbrian89@yahoo.com>
2012-08-24 16:54:26 +0300

    Added class for PayPal Billing Added Exceptions for the Billing classes

---
+++ SPL/Billing/Exception/RuntimeException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Exception for the URL class
+ *
+ * @author Brian
+ * @link https://github.com/brian978
+ * @copyright 2012
+ * @license Creative Commons Attribution-ShareAlike 3.0
+ *
+ * @name RuntimeException
+ * @version 1.0
+ *
+ */
+
+namespace SPL\Billing\Exception;
+
+class RuntimeException extends \RuntimeException implements Exception {}
---
+++ SPL/Billing/PayPal/ExpressCheckout.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * Class for PayPal's ExpressCheckout
+ *
+ * @author Brian
+ * @link https://github.com/brian978
+ * @copyright 2012
+ * @license Creative Commons Attribution-ShareAlike 3.0
+ *
+ * @name ExpressCheckout
+ * @version 1.0
+ *
+ */
+
+namespace SPL\Billing\PayPal;
+
+use SPL\Billing\Exception;
+
+class ExpressCheckout {}
