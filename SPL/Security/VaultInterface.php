fab500efff80bdde8704c7071685e324b08363cd
brian978 <dbrian89@yahoo.com>
2012-09-21 10:59:04 +0300

    Fixed some bugs Implemented the Mediator pattern for the Vault class

---
+++ SPL/Security/VaultInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ * Vault interface
+ *
+ * @author Brian
+ * @link https://github.com/brian978
+ * @copyright 2012
+ * @license Creative Commons Attribution-ShareAlike 3.0
+ *
+ */
+
+namespace SPL\Security;
+
+interface VaultInterface{}
