187012d77392b057ffe55ac972bf51be51d3e815
brian978 <dbrian89@yahoo.com>
2012-09-21 11:39:59 +0300

    Added new interface

---
+++ SPL/User/UserInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ * User interface
+ *
+ * @author Brian
+ * @link https://github.com/brian978
+ * @copyright 2012
+ * @license Creative Commons Attribution-ShareAlike 3.0
+ *
+ */
+
+namespace SPL\User;
+
+interface UserInterface{}
