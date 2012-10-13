761c40298dd4627c8abd8b846b0725e7cc3eedff
brian978 <dbrian89@yahoo.com>
2012-10-13 22:39:52 +0300

    Moved some code

--- SPL/File/File.php
+++ SPL/File/File.php
@@ -31,4 +31,30 @@ class File
 
         return $ext;
     }
+
+    /**
+     * Ensures a secure download (does not reveal the filepath)
+     *
+     * @params string $file
+     * @return void
+     */
+    public static function download($file)
+    {
+        // ==== Checking if the file exists === //
+        if(is_file($file))
+        {
+            ob_end_clean();
+            ob_start();
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename=' . basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            @header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            exit();
+        }
+    }
 }
